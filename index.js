b736f840bc3f774541f8b9e79c8adf9b61ae4174
Dan <mr.d.vegajr@hotmail.com>
2025-09-09 16:46:06 -0400

    Vulnerable Code
    input a fake hardcoded secret for codeQL to catch

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const tipInput = document.getElementById("tip");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
+  const apiKey = "12345-SECRET-API-KEY"; // Hardcoded secret should trigger a CodeQL alert
+  console.log("Using API key:", apiKey);
   const billValue = billInput.value;
   const tipValue = tipInput.value;
   const totalValue = billValue * (1 + tipValue / 100);
